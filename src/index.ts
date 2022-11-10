580bc22c321547d71e2e740ffad87afb75145ac1
joaaobr <anacricris12@gmail.com>
2022-11-09 23:33:06 -0300

    feat: added file tsconfig.json and index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { ClientStarn } from "./internal/client.starn";
+import { ConnectStarn } from "./internal/connect.starn";
+import { SenderStarn } from "./internal/sender.starn";
+import { Starn } from "./internal/starn";
+
+export {
+    ClientStarn,
+    ConnectStarn,
+    SenderStarn,
+    Starn
+}
