dcb621b2d427502a38c6b939ef954c43f7793a5a
joaaobr <anacricris12@gmail.com>
2022-11-19 14:45:20 -0300

    feat: CentralMessages and ConnectStarn removed from index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import {Client} from './src/client.starn';
-import {ConnectStarn} from './src/connect.starn';
 import {Sender} from './src/sender/sender';
-import {CentralMessages} from './src/central-messages/central-messages';
 import {Starn} from './src/starn';
 
 export {Client, Sender, Starn};
