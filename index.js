319f19087855646003fe7483713b92d0a9147f33
joaaobr <anacricris12@gmail.com>
2022-11-18 22:52:21 -0300

    fix: added javascript compatibility

---
+++ index.js
@@ -0,0 +1,5 @@
+const {Client} = require('./dist/client.starn');
+const {Sender} = require('./dist/sender/sender');
+const {Starn} = require('./dist/starn');
+
+module.exports = {Client, Sender, Starn};
