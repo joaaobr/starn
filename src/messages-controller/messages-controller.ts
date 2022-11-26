1654eb9eb471eaa051b699b0355f551437f352f3
joaaobr <anacricris12@gmail.com>
2022-11-26 18:26:03 -0300

    fix: currection in file .eslint.json

--- src/messages-controller/messages-controller.ts
+++ src/messages-controller/messages-controller.ts
@@ -1,36 +1,36 @@
-import type { ParametersStarn } from "../types/parameters-starn";
-import net from "net";
-import { SendMessage } from "./events/send-message";
-import { GetEventMessage } from "./events/get-message";
-import { MessageMenager } from "./messages/message-manager";
+import type {ParametersStarn} from '../types/parameters-starn';
+import net from 'net';
+import {SendMessage} from './events/send-message';
+import {GetEventMessage} from './events/get-message';
+import {MessageMenager} from './messages/message-manager';
 
 export class MessagesController {
-  private static readonly send: SendMessage = new SendMessage();
-  private static readonly get: GetEventMessage = new GetEventMessage(
-    MessagesController.send.getEvent()
-  );
+	private static readonly send: SendMessage = new SendMessage();
+	private static readonly get: GetEventMessage = new GetEventMessage(
+		MessagesController.send.getEvent(),
+	);
 
-  messageMenager: MessageMenager;
+	messageMenager: MessageMenager;
 
-  port: number;
-  host?: string;
-  socket: net.Server;
+	port: number;
+	host?: string;
+	socket: net.Server;
 
-  constructor(Params: ParametersStarn) {
-    if (!Params.key) {
-      Params.key = "default";
-    }
+	constructor(Params: ParametersStarn) {
+		if (!Params.key) {
+			Params.key = 'default';
+		}
 
-    this.port = Params.port;
-    this.host = Params.host;
-    this.messageMenager = new MessageMenager(Params.topics, Params.key);
+		this.port = Params.port;
+		this.host = Params.host;
+		this.messageMenager = new MessageMenager(Params.topics, Params.key);
 
-    this.socket = net
-      .createServer(async (socket) => {
-        this.messageMenager.messageMenager(socket);
+		this.socket = net
+			.createServer(async socket => {
+				this.messageMenager.messageMenager(socket);
 
-        await MessagesController.get.getEventMessage(socket);
-      })
-      .listen(this.port, this.host);
-  }
+				await MessagesController.get.getEventMessage(socket);
+			})
+			.listen(this.port, this.host);
+	}
 }
--- src/starn.ts
+++ src/starn.ts
@@ -1,39 +1,39 @@
-import { Admin } from "./admin/admin";
-import { MessagesController } from "./messages-controller/messages-controller";
-import { Client } from "./client.starn";
-import { Sender } from "./sender/sender";
-import type { ParametersStarn } from "./types/parameters-starn";
+import {Admin} from './admin/admin';
+import {MessagesController} from './messages-controller/messages-controller';
+import {Client} from './client.starn';
+import {Sender} from './sender/sender';
+import type {ParametersStarn} from './types/parameters-starn';
 
 export class Starn extends MessagesController {
-  port: number;
-  host?: string;
-  key = "default";
+	port: number;
+	host?: string;
+	key = 'default';
 
-  constructor(params: ParametersStarn) {
-    super(params);
-    if (params.key) {
-      this.key = params.key;
-    }
+	constructor(params: ParametersStarn) {
+		super(params);
+		if (params.key) {
+			this.key = params.key;
+		}
 
-    this.port = params.port;
-    this.host = params.host;
-  }
+		this.port = params.port;
+		this.host = params.host;
+	}
 
-  sender() {
-    return new Sender({
-      port: this.port,
-      host: this.host,
-    });
-  }
+	sender() {
+		return new Sender({
+			port: this.port,
+			host: this.host,
+		});
+	}
 
-  client() {
-    return new Client({
-      port: this.port,
-      host: this.host,
-    });
-  }
+	client() {
+		return new Client({
+			port: this.port,
+			host: this.host,
+		});
+	}
 
-  admin() {
-    return new Admin({ port: this.port, host: this.host }, this.key);
-  }
+	admin() {
+		return new Admin({port: this.port, host: this.host}, this.key);
+	}
 }
